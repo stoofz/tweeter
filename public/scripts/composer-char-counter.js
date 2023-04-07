216e4a0212785ebdab84af25cfbb2a14659b81ca
Anthony Chambers <ajfchambers@gmail.com>
2023-04-07 16:34:47 -0400

    Add text area counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  
+  $('#tweet-text').on('keydown', function(e) {
+
+    // Assign counter variable by id
+    const pageCounter = $('#count-value');
+
+    // Adjust count based on key down events
+    const charsLeft = pageCounter.val() - 1;
+
+    // Update counter on page
+    pageCounter.text(charsLeft);
+    if (charsLeft > 0) {
+      pageCounter.css('color', '#545149');
+    } else {
+      pageCounter.css('color', '#FF0000');
+    }
+  });
 });
