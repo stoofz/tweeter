d950e5bb772a3912d9f2c731d76d3bde120b139d
Anthony Chambers <ajfchambers@gmail.com>
2023-04-13 23:39:00 -0400

    Refactor .counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -91,7 +91,7 @@ const submitTweet = function(tweet) {
 const resetState = function() {
   $(".error-container").slideUp();
   $("#tweet-text").val('');
-  $('#count-value').text(140);
+  $('.counter').text(140);
 };
 
 // Sets error message and slides down warning
